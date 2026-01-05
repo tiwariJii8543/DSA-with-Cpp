d19aaefafb432bce57d5e9fef5981811c616790c
Harsh Tiwari <harshtiwari8543@gmail.com>
2026-01-05 20:47:10 +0530

    Refactor row and column sum implementation to use vector instead of array

--- Arrays/Multidimensional Array/rowColSum.cpp
+++ Arrays/Multidimensional Array/rowColSum.cpp
@@ -1,10 +1,10 @@
-
 #include <iostream>
+#include<vector>
 using namespace std;
 
-int main() {
-    int arr[3][3] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    // printing row wise Sum
+vector< vector<int> > rowColSum(int row, int col){
+    vector< vector<int> > arr={{0,1,2},{3,4,5},{6,7,8}};
+    // printing row wise sum
     for (int i = 0; i < 3; i++) {
         cout <<" row "<<i+1<<" : ";
         int sum=0;
@@ -24,6 +24,9 @@ int main() {
         }
         cout <<"  sum is: "<<sum<< endl;
     }
-    
-    return 0;
+    return arr;
+}
+int main(){
+    int row=3,col=3;
+    rowColSum(row,col);
 }

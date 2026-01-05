0a9ef5c9ef14cb30ea95fc296393a9aec2631b6d
Harsh Tiwari <harshtiwari8543@gmail.com>
2026-01-05 20:10:36 +0530

    Fix output label from 'col' to 'row'
    Updated output to display 'row' instead of 'col' during traversal.

--- Arrays/Multidimensional Array/traversalByRow.cpp
+++ Arrays/Multidimensional Array/traversalByRow.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int arr[3][3] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
     for (int i = 0; i < 3; i++) {
-        cout <<" col "<<i+1<<" : ";
+        cout <<" row "<<i+1<<" : ";
         for (int j = 0; j < 3; j++) {
             cout << arr[i][j] << " ";
         }

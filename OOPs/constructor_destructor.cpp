1e3040347f16291607640df6d0f76acf2979f65f
Harsh Tiwari <harshtiwari8543@gmail.com>
2026-01-17 12:13:10 +0530

    Understanding Constructor in OOPs

---
+++ OOPs/constructor_destructor.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Customer{
+    string name;
+    int acc_num;
+    int bal;
+public:
+    // default constructor
+    Customer(){
+        cout<<"Constructor called..."<<endl;   
+    }
+    // parameterized constructor
+    Customer(string a,int b,int c){
+        name=a; 
+        acc_num = b; 
+        bal=c;
+    }
+    void display(){
+        cout<<name<<" "<<acc_num<<" "<<bal<<endl;
+    }
+};
+int main(){
+    Customer A1("Harsh",101,1000);
+    A1.display();
+}p

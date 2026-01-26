830c7932b147afb27e79d2262d2eadc674e356a7
Harsh Tiwari <harshtiwari8543@gmail.com>
2026-01-26 20:04:53 +0530

    Implementing deletion at beginning and end of Linked List

---
+++ Linked List/deleteNode2.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+// deleting Node from the end of the Linked List.....
+
+class Node{
+public:
+    Node*Head=NULL;
+    int data;
+    Node*next=nullptr;
+Node(int val){
+    this->data=val;
+    this->next=next;
+}
+static Node*takeINput(){
+    int val;
+    cout<<"enter the inputs in linked list (-1 to stop): "<<endl;
+    cin>>val;
+    Node* Head = nullptr;
+    Node* tail = nullptr;
+    while (val != -1) {
+        Node* newNode = new Node(val);
+        if(Head == nullptr) {
+            Head = newNode;
+            tail = newNode;
+        }else{
+            tail->next = newNode; // Link the old tail to the new node
+            tail = newNode;       // Move tail to the new end
+        }
+        cin >> val;
+    }
+    return Head;
+}
+void printList(Node*Head){
+    Node*temp=Head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+};
+
+Node*DeleteNode(Node*Head){
+    Node*temp=Head;
+    if(Head==NULL){
+        cout<<"Node not available to delete."<<endl;
+        return Head;
+    }
+    if(Head->next==NULL){
+        delete Head;
+        return NULL;
+    }
+    while (temp->next->next != nullptr) {
+        temp=temp->next;
+    }// temp->next is the last node
+    delete temp->next; 
+    temp->next = nullptr; // This is the crucial step!
+    return Head;
+}
+int main(){
+    Node*Head=Node::takeINput();
+    Head->printList(Head);
+    Head=DeleteNode(Head);
+    Head->printList(Head);
+
+}
